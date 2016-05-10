4415749db6f958b0e0cd6e9ab48138999c5e6621
jgoley <jgoley@gmail.com>
2016-05-10 08:10:43 -0400

    Move aliases to independent file

---
+++ aliases/aliases.sh
@@ -0,0 +1,28 @@
+alias zr='exec zsh'
+
+alias rf='rm -rf'
+
+alias sa='subl3 .'
+alias atm='atom'
+alias aa='atom .'
+
+alias gaa='git add .'
+alias gbd='git branch -D'
+alias gcam='git commit -a -m '
+alias gcm='git commit -m '
+alias gpf='git push -f'
+alias grbi='git rebase -i HEAD~'
+alias gcan='git commit -v -a --no-edit --amend'
+
+alias bowi='bower install'
+alias bowis='bower install --save'
+
+alias mtr='meteor'
+alias mtra='meteor add'
+alias mtrx='meteor remove'
+alias mtrm='meteor mongo'
+alias mtru='meteor update'
+alias mtrnpm='meteor npm install --save'
+alias mtrcp='meteor create --package'
+
+alias ve='source ../venv/bin/activate'
