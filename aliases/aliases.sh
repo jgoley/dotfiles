b7ae9ba6dacd67338e4a8a07b02bcef3b38f03dd
jgoley <jgoley@gmail.com>
2016-05-16 08:08:52 -0400

    Update aliases and Atom config

--- aliases/aliases.sh
+++ aliases/aliases.sh
@@ -1,25 +1,34 @@
 . ~/code/dotfiles/setup/variables.sh
 
-alias zr='exec zsh'
-
+# Bash
 alias rf='rm -rf'
 
+# Directory
+alias dots='$dotfiles_folder'
+
+# Editors
 alias sa='subl3 .'
 alias atm='atom'
 alias aa='atom .'
 
+# Git
 alias gaa='git add .'
 alias gbd='git branch -D'
 alias gcam='git commit -a -m '
 alias gcm='git commit -m '
 alias gpf='git push -f'
-alias grbi='git rebase -i HEAD~'
 alias gcan='git commit -v -a --no-edit --amend'
-alias gcanp='git commit -v -a --no-edit --amend ; gpf'
+alias gcanp='git commit -v -a --no-edit --amend && gpf'
+gri () {
+  echo "rebase -i HEAD~$1"
+}
 
+# Npm and Bower
+alias npms='npm install --save '
 alias bowi='bower install'
 alias bowis='bower install --save'
 
+# Meteor
 alias mtr='meteor'
 alias mtra='meteor add'
 alias mtrx='meteor remove'
@@ -28,9 +37,7 @@ alias mtru='meteor update'
 alias mtrnpm='meteor npm install --save'
 alias mtrcp='meteor create --package'
 
-alias ve='source ../venv/bin/activate'
-
-alias god='sh $dotfiles_folder/util/go_dark.sh'
-alias gol='sh $dotfiles_folder/util/go_light.sh'
-
-alias dots='~/code/dotfiles'
+# Misc
+alias god='sh $dotfiles_folder/util/go_dark.sh'     # Sets dark theme on OSX, Atom and terminal
+alias gol='sh $dotfiles_folder/util/go_light.sh'    # Sets light theme on OSX, Atom and terminal
+alias zr='exec zsh'                                 # Resets oh-my-zsh
