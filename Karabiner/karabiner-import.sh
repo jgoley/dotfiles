973489bd7e604ae21dc182aaf777712903d3c3e5
jgoley <jgoley@gmail.com>
2017-10-21 19:37:39 -0400

    Fix karabiner script - use correct location of configs

--- Karabiner/karabiner-import.sh
+++ Karabiner/karabiner-import.sh
@@ -1,13 +1,27 @@
-KARABINER_DIR=~/.karabiner.d/configuration/
-KARABINER_CONFIG=$KARABINER_DIR'karabiner.json'
+. ../setup/variables.sh
+
+KARABINER_CONFIG_FILE='karabiner.json'
+KARABINER_DIR=~/.config/karabiner/
+COMPLEX_MOD_CONFIG_FILE='complex_modifications.json'
+COMPLEX_MOD_DIR=$KARABINER_DIR'assets/complex_modifications/'
 
 cson2json $DOTFILES_DIR/karabiner/complex_modifications.cson > $DOTFILES_DIR/karabiner/complex_modifications.json
 mkdir -p $KARABINER_DIR
 
-if [ ! -f $KARABINER_CONFIG ]
+# Main Config
+if [ ! -f $KARABINER_DIR$KARABINER_CONFIG_FILE ]
 then
-  ln -s $DOTFILES_DIR/karabiner/karabiner.json $KARABINER_DIR'karabiner.json'
-  echo $GREEN $CHECK "Karabiner config linked" $WHITE
+  ln -s $DOTFILES_DIR/karabiner/$KARABINER_CONFIG_FILE $KARABINER_DIR$KARABINER_CONFIG_FILE
+  echo $GREEN $CHECK $KARABINER_CONFIG_FILE' linked' $WHITE
 else
-  echo $BOLD'Karabiner config already linked'$NORM
+  echo $BOLD $KARABINER_CONFIG_FILE' already linked'$NORM
+fi
+
+# Complex Modifications Config
+if [ ! -f $COMPLEX_MOD_DIR$COMPLEX_MOD_CONFIG_FILE ]
+then
+  ln -s $DOTFILES_DIR/karabiner/$COMPLEX_MOD_CONFIG_FILE $COMPLEX_MOD_DIR$COMPLEX_MOD_CONFIG_FILE
+  echo $GREEN $CHECK $COMPLEX_MOD_CONFIG_FILE' linked' $WHITE
+else
+  echo $BOLD $COMPLEX_MOD_CONFIG_FILE' already linked'$NORM
 fi
